bddcce47a31958c7e6a9d4b3173a0f7b065a21b4
UjjwalTyagi15e <ujjwaltyagi1512@gmail.com>
2026-02-01 12:57:41 +0530

    roo

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { useState } from "react";
+import "./App.css"
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "What if I asked really nicely?",
+      "Pretty please",
+      "With a chocolate rice cake on top",
+      "What about a matcha frostie",
+      "PLEASE POOKIE",
+      "But :*(",
+      "I am going to die",
+      "Yep im dead",
+      "ok ur talking to Ujjwal's ghost",
+      "please babe",
+      ":((((",
+      "PRETTY PLEASE",
+      "Estoy muerto",
+      "No :(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you BUBU!! ;))</div>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-[200px]"
+            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+          />
+          <h1 className="my-4 text-4xl">Roo , I Know it feels like I have been a shitty guy lately . I have been hurting you again and again making you feel insecure about my love for you . I love to the absolute end of the world my cutu . You are and will always be my greatest source of support and happiness . You make me wanna get up everyday and be better than yesterday . Whatever I do I would never want to hurt you or ignore your wishes . But apart from all the explaining i stil want you to know that i am deeply sorry and feel really shamefull . Maaf krdo . Main to tumhara shota sa bachha hu na . I love you. ( i ll beat your ass if you try to find grammatical errors here Motki)    WILL YOU FORGIVE ME !!!!! </h1>
+          <div className="flex items-center">
+            <button
+              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
